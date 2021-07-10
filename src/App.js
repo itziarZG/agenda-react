cc4534aaa8d02d61df27b27ca7f82e6a973c9bd9
Itziar <izameza@ebantic.com>
2021-07-10 23:04:18 +0200

    change hashrouter to browserRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import CreateEvents from "./components/CreateEvents";
+import ListEvents from "./components/ListEvents";
+import InfoDetailEvent from "./components/InfoDetailEvent";
+import Footer from "./components/Footer";
 
 function App() {
+  const [userData, setUserData] = useState("");
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("timekids-user"));
+    if (user) {
+      setUserData(user);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header userData={userData} />
+      <Switch>
+        <Route exact path="/">
+          <ListEvents userId={userData.id} />
+        </Route>
+        <Route exact path="/events">
+          <Redirect to="/" />
+        </Route>
+
+        <Route path="/signIn">
+          <SignIn setUserData={setUserData} />
+        </Route>
+        <Route path="/signUp">
+          <SignUp setUserData={setUserData} />
+        </Route>
+        <Route path="/createEvents">
+          <CreateEvents userId={userData.id} />
+        </Route>
+        <Route path="/eventDetail/:id">
+          <InfoDetailEvent />
+        </Route>
+      </Switch>
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename="/">
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
